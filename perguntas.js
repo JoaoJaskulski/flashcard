2613f170743410cb670036d709dc63f74843b5f2
JoaoJaskulski <160859383+JoaoJaskulski@users.noreply.github.com>
2024-11-18 09:19:22 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-
-criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é Lua?',
+    'Lua é uma linguagem de programação'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Geografia',
+    'Qual a capital da Finlândia?',
+    'A capital da Finlândia é Helsinque'
 )
+
+criaCartao(
+    'Culinária',
+    'Quais são os pratos típicos da Itália?',
+    'Os pratos mais típicos da Itália são: Pizza,Ravioli, Spaghetti, Lasagna, Risotto e Arancino'
+)
+
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz romã em Inglês?',
+    'Romã em ingles é pomegranate'
 )
